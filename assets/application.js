454aa03abdd55f466e1f48ad55b33e95c9e69b64
Melvin <contactmelvinadu@gmail.com>
2022-03-13 10:57:04 -0400

    debug currency switcher and add currency conversion message to cart page

--- assets/application.js
+++ assets/application.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 
     let 
         moneySpanSelecter = 'span.money',
-        currencyPickerSelector = '[name=currencies]';
+        currencyPickerSelector = '[name=currencies]',
+        activeCurrencySelector = '.js-active-currency',
+        currencyNoteSelector = '.js-cart-currency-note';
 
     let currencyPicker = {
      loadCurrency: function() {
@@ -37,8 +39,11 @@ $(document).ready(function() {
             Currency.currentCurrency = shopCurrency;
         }
         else {
+            $(currencyPickerSelector).val(cookieCurrency);
             Currency.convertAll(shopCurrency, cookieCurrency);
         }
+
+        currencyPicker.setCurrencyText();
      },
 
      onCurrencyChanged: function(event) {
@@ -47,13 +52,34 @@ $(document).ready(function() {
             $otherPickers = $(currencyPickerSelector).not($(this));
         Currency.convertAll(Currency.currentCurrency, newCurrency);
 
+        currencyPicker.setCurrencyText(newCurrency);
+
         if ($otherPickers.length > 0) {
             $otherPickers.val(newCurrency);
         }
      },
 
+     setCurrencyText: function(newCurrency = Currency.currentCurrency) {
+        let
+            $activeCurrency = $(activeCurrencySelector),
+            $currencyNote = $(currencyNoteSelector);
+
+        if ($activeCurrency.length > 0) {
+            $activeCurrency.text(newCurrency);
+        }
+
+        if ($currencyNote.length > 0) {
+            if (newCurrency !== shopCurrency) {
+                $currencyNote.show();
+            } else {
+                $currencyNote.hide();
+            }
+        }
+     },
+
      onMoneySpanAdded: function() {
         Currency.convertAll(shopCurrency, Currency.currentCurrency);
+        currencyPicker.setCurrencyText();
      },
 
      init: function() {
