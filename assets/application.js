42aea46f540b1959f44b535917b14919571e28b8
Melvin <contactmelvinadu@gmail.com>
2022-03-24 14:13:11 -0400

    validate the add to cart form

--- assets/application.js
+++ assets/application.js
@@ -93,21 +93,88 @@ $(document).ready(function() {
      }
     }
 
-currencyPicker.init();
+    currencyPicker.init();
 
-  var original_selectCallback = window.selectCallback;
-  var selectCallback = function(variant, selector) {
-    original_selectCallback(variant, selector);
-    Currency.convertAll(shopCurrency, $('[name=currencies]').val());
-    $('.selected-currency').text(Currency.currentCurrency);
-  };
+//   var original_selectCallback = window.selectCallback;
+//   var selectCallback = function(variant, selector) {
+//     original_selectCallback(variant, selector);
+//     Currency.convertAll(shopCurrency, $('[name=currencies]').val());
+//     $('.selected-currency').text(Currency.currentCurrency);
+//   };
 
-  $('body').on('ajaxCart.afterCartLoad', function(cart) {
-    Currency.convertAll(shopCurrency, $('[name=currencies]').val());
-    $('.selected-currency').text(Currency.currentCurrency);  
-  });
+//   $('body').on('ajaxCart.afterCartLoad', function(cart) {
+//     Currency.convertAll(shopCurrency, $('[name=currencies]').val());
+//     $('.selected-currency').text(Currency.currentCurrency);  
+//   });
 
-  $('.selected-currency').text(Currency.currentCurrency);
+//   $('.selected-currency').text(Currency.currentCurrency);
+
+    //Add to Cart Form
+
+    let
+        addToCartFormSelector = '#add-to-cart-form',
+        productOptionSelector = addToCartFormSelector + ' [name*=option]';
+
+    let productForm = {
+        onProductOptionChanged: function(event) {
+            let
+                $form = $(this).closest(addToCartFormSelector),
+                selectedVariant = productForm.getActiveVariant($form)
+
+            $form.trigger('form:change', [selectedVariant])
+        },
+        getActiveVariant: function($form) {
+            let
+                variants = JSON.parse(decodeURIComponent($form.attr('data-variants')));
+                formData = $form.serializeArray();
+                formOptions = {
+                    option1: null,
+                    option2: null,
+                    option3: null
+                },
+                selectedVariant = null
+            
+            $.each(formData, function(index, item) {
+                if (item.name.indexOf('option') !== -1)  {
+                    formOptions[item.name] = item.value;
+                }
+            });
+
+            $.each(variants, function(index, variant) {
+                if (variant.option1 === formOptions.option1 && variant.option2 === formOptions.option2 && variant.option3 === formOptions.option3) {
+                    selectedVariant = variant;
+                    return false;
+                }
+            });
+
+            return selectedVariant;
+
+        },
+        validate: function(event, selectedVariant) {
+            let
+                $form = $(this),
+                hasVariant = selectedVariant !== null,
+                canAddToCart = hasVariant && selectedVariant.inventory_quantity > 0,
+                $id = $form.find('.js-variant-id'),
+                $addToCartButton = $form.find('#add-to-cart-button');
+
+            if(canAddToCart) {
+                $id.val(selectedVariant.id);
+                $addToCartButton.prop('disabled', false)
+
+            } else {
+                $id.val('');
+                $addToCartButton.prop('disabled', true)
+            }
+        },
+        init: function() {
+            $(document).on('change', productOptionSelector, productForm.onProductOptionChanged);
+            $(document).on('form:change', addToCartFormSelector, productForm.validate);
+
+        }
+    };
+
+    productForm.init();
 
 });
 
