b95847d02f242abc00c2d6b0d3ebc79bb6849f24
Melvin <contactmelvinadu@gmail.com>
2022-03-31 06:02:57 -0400

    set up the line item quantity picker

--- assets/application.js
+++ assets/application.js
@@ -273,4 +273,61 @@ $(document).ready(function() {
 
 });
 
+//Quantity Fields
+let
+    quantityFieldSelector = '.js-quantity-field',
+    quantityButtonSelector = '.js-quantity-button',
+    quantityPickerSelector = '.js-quantity-picker',
+    quantityPicker = {
+        onButtonClick: function(event){
+            //alert('Button clicked!');
+            let
+                $button = $(this),
+                $picker = $button.closest(quantityPickerSelector),
+                $quantity = $picker.find('.js-quantity-field'),
+                quantityValue = parseInt($quantity.val()),
+                max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+
+            if ($button.hasClass('plus') && (max === null || quantityValue+1 <= max)) {
+                // do something for plus click
+                $quantity.val(quantityValue + 1).change();
+            }
+            else if ($button.hasClass('minus')) {
+                // do something gor minus click
+                $quantity.val(quantityValue - 1).change();
+            }
+        },
+        onChange: function(event){
+            let 
+                $field = $(this),
+                $picker = $field.closest(quantityPickerSelector),
+                $quantityText = $picker.find('.js-quantity-text'),
+                shouldDisableMinus = parseInt(this.value) === parseInt($field.attr('min')),
+                shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
+                $minusButton = $picker.find('js-quantity-button.minus'),
+                $plusButton = $picker.find('js-quantity-button.plus');
+
+            $quantityText.text(this.value);
+
+            if (shouldDisableMinus) {
+                $minusButton.prop('disabled', true);
+            }
+            else if ($minusButton.prop('disabled') === true) {
+                $minusButton.prop('disabled', false);
+            }
+
+            if (shouldDisablePlus) {
+                $plusButton.prop('disabled', true);
+            }
+            else if ($plusButton.prop('disabled') === true) {
+                $plusButton.prop('disabled', false);
+            }
+        },
+        init: function() {
+            $(document).on('click', quantityButtonSelector, quantityPicker.onButtonClick);
+            $(document).on('change', quantityFieldSelector, quantityPicker.onChange);
+        }
+    };
+
+    quantityPicker.init();
 
